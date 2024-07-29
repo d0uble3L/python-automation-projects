7276bcc7eff24571431bf4127026b514fc66a0fb
Michael Tayo <trilltayo@Michaels-MacBook-Pro-2.local>
2024-07-28 23:20:58 -0500

    testing templatte logic

--- json_utils/update_json.py
+++ json_utils/update_json.py
@@ -1,5 +1,14 @@
 # update_json.py
 import json
+import random
+
+def generateNumbers():
+  # Generate two random integers between 1 and 100 (inclusive)
+  number1 = random.randint(1, 100)
+  number2 = random.randint(1, 100)
+  percent = number1 / number2 * 100
+
+  return percent
 
 def update_json_file(file_path, key, new_value):
   """Updates a value in a JSON file.
@@ -19,15 +28,31 @@ def update_json_file(file_path, key, new_value):
     with open(file_path, 'w') as f:
       json.dump(data, f, indent=4)
 
-    print(f"Value for key '{key}' updated successfully.")
+    print(f"Value for key '{key}' in file '{file_path}' updated successfully.")
   except FileNotFoundError:
     print(f"File '{file_path}' not found.")
   except json.JSONDecodeError:
     print(f"Error decoding JSON in file '{file_path}'.")
 
-# Example usage
-file_path = 'data.json'
-key_to_update = 'name'
-new_value = 'New Name'
+def generate_evidenceresultValue(percent):
+  # Check if the percentage is less than 95%
+  if percent > 95:
+      evidenceResultValue = "SUCCESS"
+  else:
+    evidenceResultValue = "FAILED"
 
-update_json_file(file_path, key_to_update, new_value)
+  return evidenceResultValue
+  
+
+if __name__ == "__main__":
+  percent  = generateNumbers()
+  evidenceResultValue = generate_evidenceresultValue(percent)
+  update_json_file('control.json','evidenceResult', evidenceResultValue)
+  print(percent, evidenceResultValue)
+
+
+# # Example usage
+# file_path = 'data.json'
+# key_to_update = 'name'
+# new_value = 'New Name'
+# update_json_file(file_path, key_to_update, new_value)
